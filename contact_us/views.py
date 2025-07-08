e9d9cddfc63f9c9e0777456733e113e4b261f387
Hossein <hossein.kabeh@gmail.com>
2025-07-08 12:43:19 +0330

    feat: add contact us form

--- contact_us/views.py
+++ contact_us/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django_training_project.forms import ContactForm
+
 
 def contact_us_view(request):
+    contact_form = ContactForm()
+
     context = {
-        'message2': 'this is contact us page'
+        'message2': 'this is contact us page',
+        'contact_form': contact_form,
     }
     return render(request, 'contact_us.html', context)
---
+++ django_training_project/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    full_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control form-label',
+                'placeholder': 'Full name'
+            },
+        ),
+        label=False,
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control form-label',
+            'placeholder': 'Email'
+        }),
+        label=False,
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control form-label',
+            'placeholder': 'Message'
+        }),
+        label=False,
+    )
