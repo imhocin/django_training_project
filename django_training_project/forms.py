c93564f450225a7ade2d1bfebdb4804cdf225f91
Hossein <hossein.kabeh@gmail.com>
2025-07-10 12:46:30 +0330

    chore: beautify!

--- django_training_project/forms.py
+++ django_training_project/forms.py
@@ -4,25 +4,20 @@ from django import forms
 class ContactForm(forms.Form):
     full_name = forms.CharField(
         widget=forms.TextInput(
-            attrs={
-                'class': 'form-control form-label',
-                'placeholder': 'Full name'
-            },
+            attrs={"class": "form-control form-label", "placeholder": "Full name"},
         ),
         label=False,
     )
     email = forms.EmailField(
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control form-label',
-            'placeholder': 'Email'
-        }),
+        widget=forms.EmailInput(
+            attrs={"class": "form-control form-label", "placeholder": "Email"}
+        ),
         label=False,
     )
     message = forms.CharField(
-        widget=forms.Textarea(attrs={
-            'class': 'form-control form-label',
-            'placeholder': 'Message'
-        }),
+        widget=forms.Textarea(
+            attrs={"class": "form-control form-label", "placeholder": "Message"}
+        ),
         label=False,
     )
 
@@ -31,14 +26,14 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                'class': 'form-control form-label',
+                "class": "form-control form-label",
             }
         )
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                'class': 'form-control form-label',
+                "class": "form-control form-label",
             }
         )
     )
