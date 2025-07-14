a0542c0745e1eebe65ae5847c1f74bbd5442199f
Hossein <hossein.kabeh@gmail.com>
2025-07-14 11:46:38 +0330

    feat: add register page

--- django_training_project/forms.py
+++ django_training_project/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class ContactForm(forms.Form):
@@ -37,3 +38,64 @@ class LoginForm(forms.Form):
             }
         )
     )
+
+
+User = get_user_model()
+
+
+class RegisterForm(forms.Form):
+
+    full_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-label",
+            }
+        )
+    )
+
+    user_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control form-label",
+            }
+        )
+    )
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control form-label",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control form-label",
+            }
+        )
+    )
+
+    re_password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control form-label",
+            }
+        )
+    )
+
+    def clean_user_name(self):
+        username = self.cleaned_data.get("user_name")
+        user = User.objects.filter(username=username)
+        if user.exists():
+            raise forms.ValidationError("this username is tokken")
+        return username
+
+    def clean(self):
+        data = self.cleaned_data
+        password1 = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("re_password")
+        if password1 != password2:
+            raise forms.ValidationError("password is not match!")
+        return data
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, get_user_model
+from django.shortcuts import redirect, render
 from django_training_project import forms
 
 
@@ -7,18 +7,34 @@ def login_view(request):
 
     login_form = forms.LoginForm(request.POST or None)
 
-
-
     if login_form.is_valid():
-        username = login_form.cleaned_data['username']
-        password = login_form.cleaned_data['password']
+        username = login_form.cleaned_data["username"]
+        password = login_form.cleaned_data["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            raise forms.ValidationError("Error: user not found")
+
+    context = {"page": "login", "login_form": login_form}
+
+    return render(request, "auth/login.html", context)
 
 
+User = get_user_model()
 
 
-    context = {
-        'page' : 'login',
-        'login_form': login_form,
-    }
+def register_view(request):
+    register_form = forms.RegisterForm(request.POST or None)
 
-    return render(request, 'auth/login.html', context)
+    if register_form.is_valid():
+        username = register_form.cleaned_data.get("user_name")
+        password = register_form.cleaned_data.get("password")
+        email = register_form.cleaned_data.get("email")
+        new_user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+
+    context = {"register_form": register_form}
+    return render(request, "auth/register.html", context)
