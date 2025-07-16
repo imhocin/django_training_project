5919809ce1638d7ceafec59242c717c0c3c30306
Hossein <hossein.kabeh@gmail.com>
2025-07-16 10:33:54 +0330

    feat: add base.html and extend from it

--- products/models.py
+++ products/models.py
@@ -1,12 +1,31 @@
 from django.db import models
+import random
+import os
 
 
 # Create your models here.
+
+
+def get_file_extension(filename):
+
+    base_name = os.path.basename(filename)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image(instance, filename):
+
+    name, ext = get_file_extension(filename)
+    rand = random.randint(100, 999)
+    return f"products/{instance.title}-{rand}-{name}.{ext}"
+
+
 class ProductsModel(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField(upload_to=upload_image, null=True, blank=True)
 
     def __str__(self):
         return self.title
